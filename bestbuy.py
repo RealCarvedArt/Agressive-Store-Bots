5be68a6df13bd188945939eae428cd7928b4e5e2
RealCarvedArt <78761379+RealCarvedArt@users.noreply.github.com>
2021-04-28 16:25:39 -0400

    Update bestbuy.py

--- bestbuy.py
+++ bestbuy.py
@@ -1,38 +1,51 @@
 import bs4
 import sys
 import time
-from twilio.rest import Client
-from twilio.base.exceptions import TwilioRestException
+#from twilio.rest import Client
+#from twilio.base.exceptions import TwilioRestException
+#from twilio.rest import Client
+#from twilio.base.exceptions import TwilioRestException
+#from pushbullet import Pushbullet
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, TimeoutException, \
-    WebDriverException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, TimeoutException, WebDriverException, ElementClickInterceptedException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-# ---------------------------------------------Please Read--------------------------------------------------------------
+# ========================================================= ReadMe ========================================================
 
-# Updated: 4/12/2021
+# Updated: 4/28/2021
 
 # Hello everyone! Welcome to my Best Buy script.
 # Let's go over the checklist for the script to run properly.
-#   1. Product URL
-#   2. Firefox Profile
-#   3. Credit Card CVV Number
-#   4. Twilio Account
 
-# This Script only accepts Product URL's that look like this. I hope you see the difference between page examples.
+# Install:
+# pip install beautifulsoup4
+# pip install selenium
+# pip install webdriver-manager
+
+# Optional Install:
+# pip install twilio # Must also uncomment lines at the top of this script staring with: from twilio.
+# pip install pushbullet.py # Must also uncomment the line at the top of this script staring with: from pushbullet (https://github.com/rbrcsk/pushbullet.py)
+
+#   1. Product URL
+#   2. Credit Card CVV Number
+#   3. Twilio Account (Optional)
+
+# This Script only accepts Product URL's that look like the following examples. I hope you see the difference between page examples.
 
 # Example 1 - Nvidia RTX 3080:
 # https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440
+
 # Example 2 - PS5:
 # https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149
+
 # Example 3 - Ryzen 5600x:
 # https://www.bestbuy.com/site/amd-ryzen-5-5600x-4th-gen-6-core-12-threads-unlocked-desktop-processor-with-wraith-stealth-cooler/6438943.p?skuId=6438943
 
-# This Script does not accept Product URL's that look like this.
+# This Script DOES NOT accept Product URL's that look like the following example:
 # https://www.bestbuy.com/site/searchpage.jsp?st=rtx+3080&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys
 
 # Highly Recommend To set up Twilio Account to receive text messages. So if bot doesn't work you'll at least get a phone
@@ -41,7 +54,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 # Twilio is free. Get it Here.
 # www.twilio.com/referral/BgLBXx
 
-# -----------------------------------------------Steps To Complete------------------------------------------------------
+# ======================================================= TEST LINKS ======================================================
 
 # Test Link 1 - Ryzen 5800x seems to be available quite often. It is the best URL to try out preorder script.
 # To actually avoid buying CPU, you can comment out Line 220. Uncomment the line when you are done testing.
@@ -49,37 +62,41 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 # Test Link 2 (cheap HDMI cable) - https://www.bestbuy.com/site/dynex-6-hdmi-cable-black/6405508.p?skuId=6405508
 # *Warning* - Script will try to checkout the HDMI cable twice since this is how the Bestbuy preorder script works
-# Best buy makes us click the add to cart button twice to enter Queue System. 
+# Best buy makes us click the add to cart button twice to enter Queue System.
 # Don't worry about script buying two graphics cards though. The script will only buy one.
 # As well, Best buy won't let you check out more than 1 item.
 # To actually avoid buying HDMI cable, you can comment out Line 220. Uncomment the line when you are done testing.
 
-# 1. Product URL
+# =================================================== BOT CONFIGURATION ===================================================
+
+### 1. Product URL ###
 url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
 
+### 2. Credit Card CVV Number ###
+CVV = '123'  # Enter your CVV number here in quotes.
 
-# 2. Firefox Profile
+### 3. Twilio Account (uncomment to use) ###
+#toNumber = 'your_phonenumber'
+#fromNumber = 'twilio_phonenumber'
+#accountSid = 'ssid'
+#authToken = 'authtoken'
+#client = Client(accountSid, authToken)
+
+### 4. PushBullet (uncomment to use) ###
+#token = 'YOUR_API_TOKEN_HERE'  # Get your API Token here: https://www.pushbullet.com/#settings/account (Create Access Token)
+#pb = Pushbullet(token)  # See https://github.com/rbrcsk/pushbullet.py#readme for specific device settings.
+#device = pb.get_device('YOUR DEVICE NAME HERE')  # Get your device name here: https://www.pushbullet.com/#settings/devices
+#push = device.push_note("Best Buy Alert!", "This is the body")  # You can ustomize your alert (MESSAGE TITLE and MESSAGE BODY)
+#push = device.push_link("Best Buy", url)  # You can customize your (URL TITLE and URL)
+
+# =========================================================================================================================
+
 def create_driver():
-    """Creating firefox driver to control webpage. Please add your firefox profile here."""
+    """Creating Chrome driver to control webpage."""
     options = Options()
-    options.headless = False  # Change To False if you want to see Firefox Browser Again.
-    profile = webdriver.FirefoxProfile(
-        r'C:\Users\Trebor\AppData\Roaming\Mozilla\Firefox\Profiles\t6inpqro.Robert-1613116705360')
-    web_driver = webdriver.Firefox(profile, options=options, executable_path=GeckoDriverManager().install())
+    options.headless = False  # True = Show Chrome Browser Window | False = Hide Chrome Browser Window
+    web_driver = webdriver.Chrome(ChromeDriverManager().install())
     return web_driver
-
-
-# 3. credit card CVV Number
-CVV = '123'  # You can enter your CVV number here in quotes.
-
-# 4. Twilio Account
-toNumber = 'your_phonenumber'
-fromNumber = 'twilio_phonenumber'
-accountSid = 'ssid'
-authToken = 'authtoken'
-client = Client(accountSid, authToken)
-
-# ----------------------------------------------------------------------------------------------------------------------
 
 
 def time_sleep(x, driver):
@@ -196,7 +213,7 @@ def searching_for_card(driver):
                     searching_for_card(driver)
 
                 # Logging Into Account.
-                print("Attempting to Login. Firefox should remember your login info to auto login.")
+                print("Attempting to Login. Chrome should remember your login info to auto login.")
 
                 # Click Shipping Option. (if available)
                 try:
@@ -224,7 +241,7 @@ def searching_for_card(driver):
                     wait2.until(EC.presence_of_element_located((By.XPATH, "//*[@class='btn btn-lg btn-block btn-primary button__fast-track']")))
                     print("clicked checkout")
                     # comment the line down below to avoid buying when testing bot. vv
-                    driver_click(driver, 'xpath', 'btn btn-lg btn-block btn-primary button__fast-track')  
+                    driver_click(driver, 'xpath', 'btn btn-lg btn-block btn-primary button__fast-track')
                 except (NoSuchElementException, TimeoutException, ElementNotInteractableException):
                     print("Could Not Complete Checkout.")
 
